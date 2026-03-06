07e8fef2423e63b5c8ee266a447738a83a51b399
6uiwj <oterges00@gmail.com>
2026-03-06 22:02:34 +0900

    dataType

---
+++ dataType.py
@@ -0,0 +1,57 @@
+#숫자형
+#제곱
+a = 3
+b = 4
+print(a**b)
+
+#나누기
+print(7/4)
+
+#몫
+print(7//4)
+
+
+#문자형
+
+a=3
+b=4
+print(a+b)
+
+multililne = """
+Lfe is too short
+you need python"""
+
+print(multililne)
+
+#문자열 연산
+#문자열 더하기
+head = "Python"
+tail = "is fun!"
+print(head+tail)
+
+#문자열 곱하기
+a = "python"
+print(a*2)
+
+#문자열 길이 구하기
+a = "Life is too short"
+print(len(a))
+
+sentence = "You need python"
+print(sentence)
+
+#문자열 인덱싱
+a = "Life is too short, You need Python"
+print(a[3]) #e
+
+#문자열 인덱싱 활용하기
+print(a[0]) #L
+print(a[12]) #s
+print(a[-1]) #n - 뒤에서부터 읽기
+print(a[-2]) #o
+print(a[-5]) #y
+
+#문자열 슬라이싱 [x,y)
+b = a[0] + a[1] + a[2] + a[3]
+print(b) #Life
+print(a[0:4]) #Life
