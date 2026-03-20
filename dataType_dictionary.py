0d42e251fa176b566eabceeb106bc049fcbe5fff
6uiwj <oterges00@gmail.com>
2026-03-20 21:10:31 +0900

    dataType-dictionary2

--- dataType_dictionary.py
+++ dataType_dictionary.py
@@ -17,3 +17,29 @@ print(a)
 
 a[3] = [1,2,3]
 print(a)
+
+#딕셔너리 요소 삭제하기
+del a[1]
+print(a)
+
+#딕셔너리에서 key를 사용해 Value 얻기
+grade = {'pey' :10, 'julliet':99 }
+print(grade['pey'])
+print(grade['julliet'])
+
+a = {1:'a', 2:'b'}
+print(a[1])
+
+
+dic = {'name':'pey', 'phone':'010-9999-1234', 'birth': '1118'}
+print(dic['name'])
+print(dic['phone'])
+print(dic['birth'])
+
+#딕셔너리를 만들 때 주의사항
+##key는 고유값, 중복된 key값을 설정하면 하나를 제외한 나머지의 것들이 모두 무시된다.
+a = { 1:'a', 1:'b' }
+print(a) #{1: 'b'}
+
+
+##key는 불변값 => list 불가능 / 튜플 가능
