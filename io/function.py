7b1705f13ab1e9cc1b54077e5b555caea9beef9a
6uiwj <oterges00@gmail.com>
2026-04-05 23:38:22 +0900

    io-function2

--- io/function.py
+++ io/function.py
@@ -66,3 +66,61 @@ print(result)
 
 result = add_many(1,2,3,4,5,6,7,8,9,10)
 print(result)
+
+def add_mul(choice, *args):
+    if choice == "add":
+        result = 0
+        for i in args:
+            result = result + i
+    elif choice == "mul":
+        result = 1
+        for i in args:
+            result = result * i
+    return result
+
+result = add_mul('add', 1,2,3,4,5)
+print(result)
+
+
+result = add_mul('mul', 1,2,3,4,5)
+print(result)
+
+#키워드 매개변수, kwargs -> 딕셔너리 형태로 저장
+def print_kwargs(**kwargs):
+    print(kwargs)
+
+print_kwargs(a=1)
+print_kwargs(name='foo', age=3)
+
+#함수의 리턴값은 언제나 하나이다
+def add_and_mul(a,b):
+    return a+b, a*b
+
+result = add_and_mul(3,4)
+print(result) #튜플로리턴 -> 7,12
+
+result1, result2 = add_and_mul(3,4) #각각 리턴
+print(result1)
+print(result2)
+
+def add_and_mul(a,b):
+    return a + b
+    return a * b #함수는 리턴값이 하나이므로 반영되지 않음
+
+##return의 또다른 쓰임새
+def say_nick(nick):
+    if nick == "바보":
+        return
+    print("나의 별명은 %s입니다." % nick)
+
+say_nick('야호')
+say_nick('바보') #return이 실행되어 함수를 빠져나옴
+
+#매개변수에 초깃값 미리 설정하기
+def say_myself(name, age, man=True):
+    print("나의 이름은 %s입니다." % name)
+    print("나이는 %d살입니다." % age)
+    if man:
+        print("남자입니다.")
+    else:
+        print("여자입니다.")
