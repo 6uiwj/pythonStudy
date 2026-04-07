79d914e29e665cf56e23dcfe923d5139187e4186
6uiwj <oterges00@gmail.com>
2026-04-07 19:33:21 +0900

    io-user io

---
+++ io/userIo.py
@@ -0,0 +1,19 @@
+#사용자 입력 활용하기
+##input 활용하기
+#a = input() #문자 입력
+#print(a)
+
+#number = input("숫자를 입력하세요: ")
+#print(number)
+
+#print 자셓 ㅣ알기
+##큰따옴표로 둘러싸인 문자열은 + 연산과 동일하다
+print("life" "is" "too short")
+print("life"+"is"+"too short")
+
+#문자열 띄어쓰기는 쉼표로 한다.
+print("life", "is", "too short")
+
+#한줄에 결괏값 출력하기
+for i in range(10):
+    print(i, end= ' ')
