8d6afdf1d0e03cc04a85757897ae87f4b213be88
6uiwj <oterges00@gmail.com>
2026-04-17 13:47:51 +0900

    intensified-module

---
+++ intensified/module/mod1.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+if __name__ == "__main__": #대화형 인터프리터 혹은 파일에서 모듈 호출시 거짓으로 취급
+    print(add(1,4))
+    print(add(4,2))
---
+++ intensified/module/modtest.py
@@ -0,0 +1,4 @@
+#다른파일에서 모듈 불러오기
+# import mod2
+# result = mod2.add(3,4)
+# print(result)
